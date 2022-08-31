2c43756a529f67d61be38997a80910891517a72f
Brian Abidin <Brian.abidin1@gmail.com>
2022-08-31 16:29:12 -0400

    added image height change when resize window

--- form.js
+++ form.js
@@ -1,13 +1,17 @@
 const form = document.querySelector(".form")
 const header = document.querySelector(".header")
+const image = document.querySelector("img")
 
 const observer = new ResizeObserver(entries => {
 console.log(entries)
     const formElement = entries[0]
     const headerElement = entries[1]
+    const imageElement = entries[2]
     const isResized = formElement.contentRect.height > 350
     headerElement.target.style.paddingTop = isResized ? "0vh" : "24vh"
+    imageElement.target.style.height = isResized ? "100vh" : "124vh"
 })
 
 observer.observe(form)
 observer.observe(header)
+observer.observe(image)
