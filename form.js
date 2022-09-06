6502bb1c89bc3f5196790b52af40fd20a9e319e0
Brian Abidin <Brian.abidin1@gmail.com>
2022-09-06 14:38:04 -0400

    added event listener that turns input border red if invalid on submit

--- form.js
+++ form.js
@@ -1,19 +1,19 @@
-const form = document.querySelector(".form")
-const header = document.querySelector(".header")
+const form = document.querySelector(".formbg")
+const first = document.querySelector(".first")
 const image = document.querySelector("img")
 
 const observer = new ResizeObserver(entries => {
 console.log(entries)
     const formElement = entries[0]
-    const headerElement = entries[1]
+    const firstElement = entries[1]
     const imageElement = entries[2]
     const isResized = formElement.contentRect.height > 350
-    headerElement.target.style.paddingTop = isResized ? "0vh" : "24vh"
-    imageElement.target.style.height = isResized ? "100vh" : "124vh"
+    firstElement.target.style.paddingTop = isResized ? "0vh" : "24vh"
+    imageElement.target.style.height = isResized ? "100vh" : "100vh"
 })
 
 observer.observe(form)
-observer.observe(header)
+observer.observe(first)
 observer.observe(image)
 
 function verifyPassword(){
@@ -25,3 +25,7 @@ function verifyPassword(){
         confirm.setCustomValidity('Passwords do not match');
     }
 }
+
+document.getElementById("submitbtn").addEventListener("click", function(){
+    document.getElementById("form").className="submitted";
+})
